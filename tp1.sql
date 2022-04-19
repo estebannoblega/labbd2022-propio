856c10c8d4b9016df8be2d558949589f595c0371
EstebanNoblega <estebannoblega@gmail.com>
2022-04-19 12:24:31 -0300

    ultimas versiiones de los archivos

--- tp1.sql
+++ tp1.sql
@@ -2,7 +2,7 @@
 -- Grupo Nro: 08
 -- Integrantes: Faciano Enzo Nicolás, Nóblega Caro Esteban Leonel
 -- Tema: Veterinaria
--- Nombre del Esquema: LBD2022G08Veterinaria
+-- Nombre del Esquema: LBD2022G08
 -- Plataforma: Windows 10
 -- GitHub Repositorio: LBD2022G08
 -- GitHub Usuarios: mezuckerberg, estebannoblega
@@ -27,7 +27,7 @@ CREATE TABLE IF NOT EXISTS `LBD2022G08`.`Personas` (
 ENGINE = InnoDB;
 -- DEFAULT CHARACTER SET = utf8;
 
-CREATE TABLE IF NOT EXISTS `LBD2022G08`.`Usuarios` (
+CREATE TABLE IF NOT EXISTS `LBD2022G08`.`Clientes` (
   `IdPersona` INT(11) NOT NULL,
   `FechaNacimiento` DATE NOT NULL CHECK (FechaNacimiento< '2005-01-01' ) /*Comprobar que la persona sea mayor de 18 años*/,
   PRIMARY KEY (`IdPersona`),
@@ -51,41 +51,40 @@ ENGINE = InnoDB;
 ALTER TABLE `LBD2022G08`.`InfoContacto` MODIFY Telefono INT NOT NULL;
 
 CREATE TABLE IF NOT EXISTS `LBD2022G08`.`Ventas` (
-  `idVentas` INT(11) NOT NULL,
-  `IdPersona` INT(11) NOT NULL,
-  `NumeroVenta` INT(10) NOT NULL,
-  `FechaVenta` DATE NOT NULL,
-  PRIMARY KEY (`idVentas`, `IdPersona`),
+  `idVenta` INT NOT NULL AUTO_INCREMENT,
+  `IdPersona` INT NOT NULL,
+  `NumeroVenta` INT NOT NULL,
+  `FechaVenta` DATETIME NOT NULL,
+  PRIMARY KEY (`idVenta`, `IdPersona`),
   UNIQUE INDEX `Venta_UNIQUE` (`NumeroVenta` ASC),
   INDEX `fk_Ventas_Usuarios1_idx` (`IdPersona` ASC),
   CONSTRAINT `fk_Ventas_Usuarios1`
     FOREIGN KEY (`IdPersona`)
-    REFERENCES `LBD2022G08`.`Usuarios` (`IdPersona`)
+    REFERENCES `LBD2022G08`.`Clientes` (`IdPersona`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
--- DEFAULT CHARACTER SET = utf8;
 
 CREATE TABLE IF NOT EXISTS `LBD2022G08`.`Proveedores` (
-  `idProveedores` INT(11) NOT NULL,
+  `idProveedor` INT(11) NOT NULL,
   `CUIT` INT(11) NOT NULL,
   `Domicilio` VARCHAR(45) NOT NULL,
   `Email` VARCHAR(30) NOT NULL,
   `RazonSocial` VARCHAR(30) NOT NULL,
   `Telefono` INT(11) NOT NULL,
   `Estado` ENUM('A', 'I') NOT NULL  /*A: Activo - I: Inactivo*/,
-  PRIMARY KEY (`idProveedores`),
+  PRIMARY KEY (`idProveedor`),
   UNIQUE INDEX `CUIT&RazonSocial_UNIQUE` (`CUIT` ASC, `RazonSocial` ASC))
 ENGINE = InnoDB;
 -- DEFAULT CHARACTER SET = utf8;
+
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
--- Corrección del nombre de la tabla usuarios, se debería llamar Clientes
-ALTER TABLE `LBD2022G08`.`Usuarios` RENAME	`Clientes`;
+
 -- Carga de Datos Personas - Administrador
 INSERT INTO Personas (Nombre,Apellido,Email,Usuario,Contrasenia,Estado,Tipo) VALUES ('Bryan','Millis','bryanmillis@hotmail.com','administrador','1234','Activo','A');
 -- Carga de Datos Personas - Usuarios
@@ -108,7 +107,7 @@ INSERT INTO Personas (Nombre,Apellido,Email,Usuario,Contrasenia,Estado,Tipo) VAL
 INSERT INTO Personas (Nombre,Apellido,Email,Usuario,Contrasenia,Estado,Tipo) VALUES ('Bobby','Fischer','bobbyfish@gmail.com','TM','ajedrez','Activo','U');
 INSERT INTO Personas (Nombre,Apellido,Email,Usuario,Contrasenia,Estado,Tipo) VALUES ('Boris','Spassky','boris@outlook.com.ru','BS','ajdz','Activo','U');
 
-select * from personas;
+select * from personas where Estado = 'Activo';
 
 -- Carga de datos de usuarios
 INSERT INTO Clientes VALUES (2,'1973-06-11');
@@ -135,5 +134,27 @@ SELECT * FROM Clientes;
 
 -- Carga de datos para infocontacto
 INSERT INTO InfoContacto (Direccion, Telefono, Email) VALUES ('Lavalle 150',3815497552,'ventas@veterinariavetnet.com');
+
+-- Carga de datos para ventas
+INSERT INTO Ventas (IdPersona, NumeroVenta, FechaVenta) VALUES (2,48692039,'2021-07-19 16:42:06');
+INSERT INTO Ventas (IdPersona, NumeroVenta, FechaVenta) VALUES (2,48692040,'2021-05-27 18:00:35');
+INSERT INTO Ventas (IdPersona, NumeroVenta, FechaVenta) VALUES (6,48692041,'2021-05-27 18:30:35');
+INSERT INTO Ventas (IdPersona, NumeroVenta, FechaVenta) VALUES (7,48692042,'2021-02-16 00:18:47');
+INSERT INTO Ventas (IdPersona, NumeroVenta, FechaVenta) VALUES (2,48692043,'2021-05-27 18:00:35');
+INSERT INTO Ventas (IdPersona, NumeroVenta, FechaVenta) VALUES (14,48692044,'2020-05-05 13:49:46');
+INSERT INTO Ventas (IdPersona, NumeroVenta, FechaVenta) VALUES (16,48692045,'2022-02-14 03:40:46');
+INSERT INTO Ventas (IdPersona, NumeroVenta, FechaVenta) VALUES (16,48692046,'2021-08-04 23:48:16');
+INSERT INTO Ventas (IdPersona, NumeroVenta, FechaVenta) VALUES (17,48692047,'2022-08-08 18:46:21');
+INSERT INTO Ventas (IdPersona, NumeroVenta, FechaVenta) VALUES (17,48692048,'2022-11-12 03:41:23');
+INSERT INTO Ventas (IdPersona, NumeroVenta, FechaVenta) VALUES (16,48692049,'2022-12-02 23:42:31');
+INSERT INTO Ventas (IdPersona, NumeroVenta, FechaVenta) VALUES (20,48692050,'2022-10-03 18:21:30');
+INSERT INTO Ventas (IdPersona, NumeroVenta, FechaVenta) VALUES (14,48692051,'2022-01-20 15:50:02');
+INSERT INTO Ventas (IdPersona, NumeroVenta, FechaVenta) VALUES (19,48692052,'2022-11-19 01:08:07');
+INSERT INTO Ventas (IdPersona, NumeroVenta, FechaVenta) VALUES (19,48692053,'2021-06-30 15:24:25');
+INSERT INTO Ventas (IdPersona, NumeroVenta, FechaVenta) VALUES (17,48692054,'2022-12-13 15:30:37');
+INSERT INTO Ventas (IdPersona, NumeroVenta, FechaVenta) VALUES (17,48692055,'2022-05-27 21:15:36');
+INSERT INTO Ventas (IdPersona, NumeroVenta, FechaVenta) VALUES (6,48692056,'2020-01-11 11:41:18');
+INSERT INTO Ventas (IdPersona, NumeroVenta, FechaVenta) VALUES (6,48692057,'2020-10-04 07:43:26');
+INSERT INTO Ventas (IdPersona, NumeroVenta, FechaVenta) VALUES (6,48692058,'2021-09-10 07:07:49');
 
 -- Carga de datos para Proveedores
